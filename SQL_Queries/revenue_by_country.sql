4b19ff5895603145bc7fbe845b9e0d308a1fb631
Data_13 <joe@jadamski.com>
2025-11-06 13:06:08 +0100

    Update revenue_by_country.sql

--- SQL_Queries/revenue_by_country.sql
+++ SQL_Queries/revenue_by_country.sql
@@ -1,1 +1,20 @@
+/*
+--------------------------------------------------------
+Query: Revenue by Country
+Purpose: Display the top 10 countries ranked by total revenue.
+Source: Adapted from Task 3.7 Top 10 Countries by Customer Count
+Concepts: Multi-table Joins, Aggregation, Ranking
+--------------------------------------------------------
+*/
 
+SELECT
+    co.country,
+    SUM(p.amount) AS total_revenue
+FROM payment p
+JOIN customer c ON p.customer_id = c.customer_id
+JOIN address a ON c.address_id = a.address_id
+JOIN city ci ON a.city_id = ci.city_id
+JOIN country co ON ci.country_id = co.country_id
+GROUP BY co.country
+ORDER BY total_revenue DESC
+LIMIT 10;
