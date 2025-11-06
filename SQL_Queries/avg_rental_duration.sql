7cb529ac7530bd0a04c6ecd92ed5cdb7d07b9258
Data_13 <joe@jadamski.com>
2025-11-06 13:04:19 +0100

    Update avg_rental_duration.sql

--- SQL_Queries/avg_rental_duration.sql
+++ SQL_Queries/avg_rental_duration.sql
@@ -1,1 +1,14 @@
+/*
+--------------------------------------------------------
+Query: Average Rental Duration
+Purpose: Calculate the minimum, maximum, and average film lengths.
+Source: Task 3.6 Film Length Summary
+Concepts: Aggregation (MIN, MAX, AVG)
+--------------------------------------------------------
+*/
 
+SELECT
+    MIN(length) AS min_length,
+    MAX(length) AS max_length,
+    AVG(length) AS avg_length
+FROM film;
